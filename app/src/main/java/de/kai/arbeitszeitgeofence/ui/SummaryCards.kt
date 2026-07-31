33be0ac82f87a00f19bba20e37da9c0a89a38f51
Kai Becker <tazzy0703@gmail.com>
2026-07-31 09:45:37 +0200

    Fix SummaryCards dp import

--- app/src/main/java/de/kai/arbeitszeitgeofence/ui/SummaryCards.kt
+++ app/src/main/java/de/kai/arbeitszeitgeofence/ui/SummaryCards.kt
@@ -10,7 +10,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.unit.dp
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun SummaryCard(
